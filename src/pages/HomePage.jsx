c3eb50c37f9162aa723c918a657ec601f5be60b7
itzaur <preneur85@gmail.com>
2024-10-21 13:46:50 +0300

    feat: fetching listings in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,44 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import {
-  isListingAvailable,
-  listings as staticListings,
-} from '@/api/data/listings';
-import ListingFilters from '@/components/ListingFilters';
+import api from '@/api';
+import ListingFilters from '@/components/ListingFilters.jsx';
 import ListingList from '@/components/ListingList.jsx';
 import { Separator } from '@/components/ui';
 
 const HomePage = () => {
-  const [listings, setListings] = useState(staticListings);
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const response = await api.get('/api/listings');
+
+      setListings(response.data);
+    };
+
+    fetchListings();
+  }, []);
 
   const handleFilters = (filters) => {
-    const { dates, guests, search } = filters;
-
-    // Resets filters by using static listings
-    let filteredListings = staticListings;
-
-    // Handles date range
-    if (dates) {
-      filteredListings = filteredListings.filter((listing) =>
-        isListingAvailable(listing, dates),
-      );
-    }
-
-    // Handles guests
-    if (guests) {
-      filteredListings = filteredListings.filter(
-        (listing) => guests <= listing.maxGuests,
-      );
-    }
-
-    // Handles search
-    if (search) {
-      filteredListings = filteredListings.filter((listing) =>
-        listing.name.toLowerCase().includes(search.toLowerCase()),
-      );
-    }
-
-    setListings(filteredListings);
+    // const { dates, guests, search } = filters;
+    // // Resets filters by using static listings
+    // let filteredListings = staticListings;
+    // // Handles date range
+    // if (dates) {
+    //   filteredListings = filteredListings.filter((listing) =>
+    //     isListingAvailable(listing, dates),
+    //   );
+    // }
+    // // Handles guests
+    // if (guests) {
+    //   filteredListings = filteredListings.filter(
+    //     (listing) => guests <= listing.maxGuests,
+    //   );
+    // }
+    // // Handles search
+    // if (search) {
+    //   filteredListings = filteredListings.filter((listing) =>
+    //     listing.name.toLowerCase().includes(search.toLowerCase()),
+    //   );
+    // }
+    // setListings(filteredListings);
   };
 
   return (
