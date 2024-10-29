cd55830f17e679645ba8256af44158f4a95964a1
itzaur <preneur85@gmail.com>
2024-10-29 15:19:15 +0300

    refactor: create DataRenderer component, update HomePage and ListingDetailsPage with DataRenderer

--- src/pages/ListingDetailsPage.jsx
+++ src/pages/ListingDetailsPage.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import api from '@/api';
+import DataRenderer from '@/components/DataRenderer';
 import ListingDetailsCard from '@/components/ListingDetailsCard';
-import { Spinner } from '@/components/ui';
 import useFetch from '@/hooks/useFetch';
 
 const ListingDetailsPage = () => {
@@ -15,23 +13,13 @@ const ListingDetailsPage = () => {
     isLoading,
   } = useFetch(`/api/listings/${listingId}`);
 
-  const renderListing = () => {
-    if (isLoading) {
-      return (
-        <div className='flex justify-center'>
-          <Spinner size='sm' />
-        </div>
-      );
-    }
-
-    if (error) {
-      return <div className='text-center'>{error}</div>;
-    }
-
-    return <ListingDetailsCard listing={listing} />;
-  };
-
-  return <div className='container py-4'>{renderListing()}</div>;
+  return (
+    <div className='container py-4'>
+      <DataRenderer error={error} isLoading={isLoading}>
+        <ListingDetailsCard listing={listing} />
+      </DataRenderer>
+    </div>
+  );
 };
 
 export default ListingDetailsPage;
