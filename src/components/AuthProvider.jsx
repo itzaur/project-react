0e6caceb0f6db478baecbdd4b82b2d6e4016e159
itzaur <preneur85@gmail.com>
2025-01-06 16:16:34 +0300

    feat: create AuthProvider component

---
+++ src/components/AuthProvider.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+import api from '@/api';
+
+const AuthContext = createContext(undefined);
+
+export const useAuth = () => {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return authContext;
+};
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const response = await api.get('/api/me');
+        setToken(response.data.accessToken);
+      } catch {
+        setToken(null);
+      }
+    };
+
+    fetchMe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
